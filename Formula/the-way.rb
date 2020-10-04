4aa5b947f7b35cc00e84d90b796b0699fbf80ff8
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-10-04 13:31:08 +0200

    Update the-way.rb

--- Formula/the-way.rb
+++ Formula/the-way.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class The-Way < Formula
+class TheWay < Formula
   desc "A code snippets manager for your terminal"
   homepage "https://github.com/out-of-cheese-error/the-way"
   url "https://github.com/out-of-cheese-error/the-way/releases/latest/download/the-way-mac.tar.gz"
