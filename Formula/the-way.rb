1ee9fb06adcdb1d1632b01aef8e848793c3912bb
Ninjani <48680156+Ninjani@users.noreply.github.com>
2021-06-22 15:25:44 +0200

    build(version): update

--- Formula/the-way.rb
+++ Formula/the-way.rb
@@ -4,9 +4,9 @@
 class TheWay < Formula
   desc "A code snippets manager for your terminal"
   homepage "https://github.com/out-of-cheese-error/the-way"
-  url "https://github.com/out-of-cheese-error/the-way/releases/download/v0.13.0/the-way-v0.13.0-x86_64-apple-darwin.tar.gz"
-  sha256 "e4e86cff2e632ef15acbcece2fab9138ed50b2ba3deb7a58095fe114db10e579"
-  version "0.13.0"
+  url "https://github.com/out-of-cheese-error/the-way/releases/download/v0.14.0/the-way-v0.14.0-x86_64-apple-darwin.tar.gz"
+  sha256 "a442d242899d1f798410369622080b12dea04871c66967db8bf7c15c8b74bddd"
+  version "0.14.0"
 
   def install
     bin.install "the-way"
