f81781e284452e9e7f23cfbbb45ef5498c1efbf6
Ninjani <janani.durairaj@gmail.com>
2023-01-17 21:06:51 +0100

    build(version): update

--- Formula/the-way.rb
+++ Formula/the-way.rb
@@ -4,9 +4,9 @@
 class TheWay < Formula
   desc "A code snippets manager for your terminal"
   homepage "https://github.com/out-of-cheese-error/the-way"
-  url "https://github.com/out-of-cheese-error/the-way/releases/download/v0.17.0/the-way-macos.tar.gz"
-  sha256 "95dc15938f4e7096ccd0f984ab6ca34116e0457a76101d7a4ee0c923f3f3e5b0"
-  version "0.17.0"
+  url "https://github.com/out-of-cheese-error/the-way/releases/download/v0.19.0/the-way-macos.tar.gz"
+  sha256 "9d87becf74e5829189af3c61fefe42034cf90beec41955a225055cc8917b9fc5"
+  version "0.19.0"
 
   def install
     bin.install "the-way"
